b74ab0c67208a7f9607d8011732c81be6102382f
Richard Quayson <richard.quayson@ashesi.edu.gh>
2024-03-13 02:32:57 +0000

    updated Course pydantic to normalise timerange

--- test/struct.py
+++ test/struct.py
@@ -1,17 +1,18 @@
 from pydantic import BaseModel
 from typing import List, Optional
-
+from datetime import time
 
 class Semester(BaseModel):
     name: str
     year: int
 
+class TimeRange(BaseModel):
+    start_time: time
+    end_time: time
 
 class OfficeHour(BaseModel):
     day: str            # e.g. Monday
-    start_time: str
-    end_time: str
-
+    time: TimeRange
 
 class Instructor(BaseModel):
     name: str
@@ -19,13 +20,13 @@ class Instructor(BaseModel):
     office_hours: List[OfficeHour]
     phone: Optional[str]
 
-
 class LectureDay(BaseModel):
     day: str            # e.g. Monday
-    start_time: str
-    end_time: str
+    time: TimeRange
     location: str
 
+class WeeklySchedule(BaseModel):
+    pass
 
 class Course(BaseModel):
     code: str
@@ -35,4 +36,4 @@ class Course(BaseModel):
     instructor: Instructor
     faculty_intern: Instructor
     lecture_days: List[LectureDay]
-    # weekly_schedule: WeeklySchedule
+    weekly_schedule: WeeklySchedule
