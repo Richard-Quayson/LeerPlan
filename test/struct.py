1e3e171cf8cc44d6cf3ead26e39a8d8a3029b54d
Richard Quayson <richard.quayson@ashesi.edu.gh>
2024-03-15 15:11:04 +0000

    added Pydantic classes for EvaluationCriteria and WeeklySchedule

--- test/struct.py
+++ test/struct.py
@@ -1,6 +1,6 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, PositiveInt, PositiveFloat
 from typing import List, Optional
-from datetime import time
+from datetime import time, date
 
 class Semester(BaseModel):
     name: str
@@ -20,12 +20,41 @@ class Instructor(BaseModel):
     office_hours: List[OfficeHour]
     phone: Optional[str]
 
+class Textbook(BaseModel):
+    title: str
+    type: Optional[str]
+
+class EvaluationCriteria(BaseModel):
+    type: str
+    weight: PositiveFloat
+
 class LectureDay(BaseModel):
     day: str            # e.g. Monday
     time: TimeRange
     location: str
 
+class Topic(BaseModel):
+    title: str
+
+class Chapter(BaseModel):
+    chapter: str
+
+class Assessment(BaseModel):
+    name: str
+    due_date: Optional[date]
+    weight: Optional[float]
+
+class Week(BaseModel):
+    week_number: PositiveInt
+    start_date: Optional[date]
+    end_date: Optional[date]
+    type: str
+    topics: List[Topic]
+    reading: List[Chapter]
+    assessments: Optional[List[Assessment]]
+
 class WeeklySchedule(BaseModel):
+    weeks: list[Week]
     pass
 
 class Course(BaseModel):
@@ -35,5 +64,7 @@ class Course(BaseModel):
     semester: Semester
     instructor: Instructor
     faculty_intern: Instructor
+    textbooks: List[Textbook]
+    evaluation_criteria: List[EvaluationCriteria]
     lecture_days: List[LectureDay]
     weekly_schedule: WeeklySchedule
