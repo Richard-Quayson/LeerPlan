f7bbb9b6679ecbaaa130a96b07cbede89fef58d1
Richard Quayson <richard.quayson@ashesi.edu.gh>
2024-05-03 13:47:16 +0000

    adding view to register account

--- leerplan_api/Account/views.py
+++ leerplan_api/Account/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import status, generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny, IsAuthenticated
 
-# Create your views here.
+from .models import UserAccount, AccessTokenBlacklist
+from .serializers import (AccountRegistrationSerializer, AccountLoginSerializer, UserAccountSerializer,
+                          ChangePasswordSerializer)
+from .permissions import IsAccessTokenBlacklisted
+
+
+class AccountRegistrationView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        serializer = AccountRegistrationSerializer(data=request.data)
+
+        if serializer.is_valid():
+            account = serializer.save()
+            return Response(UserAccountSerializer(account).data, status=status.HTTP_201_CREATED)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
