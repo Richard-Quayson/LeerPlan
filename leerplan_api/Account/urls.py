cb2aea9a883a58af29cbca4b90f67f6c47d75711
Richard Quayson <richard.quayson@ashesi.edu.gh>
2024-05-03 14:17:30 +0000

    configuring route for retrieving authenticated account details

--- leerplan_api/Account/urls.py
+++ leerplan_api/Account/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import (AccountRegistrationView, AccountLoginView)
+from .views import (AccountRegistrationView, AccountLoginView, AccountView)
 
 urlpatterns = [
     path("register/", AccountRegistrationView.as_view(), name="register-user"),
     path("login/", AccountLoginView.as_view(), name="login-user"),
     path("token/refresh/", TokenRefreshView.as_view(), name="refresh-access-token"),
+    path("user", AccountView.as_view(), name="account-details"),
 ]
