1bac257323706661c54c47b9869ee1da926fc16c
Richard Quayson <richard.quayson@ashesi.edu.gh>
2024-05-03 14:10:33 +0000

    configuring route for account login

--- leerplan_api/Account/urls.py
+++ leerplan_api/Account/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import (AccountRegistrationView, )
+from .views import (AccountRegistrationView, AccountLoginView)
 
 urlpatterns = [
     path("register/", AccountRegistrationView.as_view(), name="register-user"),
+    path("login/", AccountLoginView.as_view(), name="login-user"),
 ]
