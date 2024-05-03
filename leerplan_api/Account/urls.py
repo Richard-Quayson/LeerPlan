47cfa1588a0cd2c370aa85934c430f128fa09089
Richard Quayson <richard.quayson@ashesi.edu.gh>
2024-05-03 14:12:35 +0000

    configuring route for refreshing access token after it expires

--- leerplan_api/Account/urls.py
+++ leerplan_api/Account/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 from .views import (AccountRegistrationView, AccountLoginView)
 
 urlpatterns = [
     path("register/", AccountRegistrationView.as_view(), name="register-user"),
     path("login/", AccountLoginView.as_view(), name="login-user"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="refresh-access-token"),
 ]
