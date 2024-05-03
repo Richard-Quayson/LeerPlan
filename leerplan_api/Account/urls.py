c3b74afbac709c2e00b62072b685b099230777f9
Richard Quayson <richard.quayson@ashesi.edu.gh>
2024-05-03 14:23:36 +0000

    configuring route for changing account password

--- leerplan_api/Account/urls.py
+++ leerplan_api/Account/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import (AccountRegistrationView, AccountLoginView, AccountView)
+from .views import (AccountRegistrationView, AccountLoginView, AccountView,
+                    ChangePasswordView)
 
 urlpatterns = [
     path("register/", AccountRegistrationView.as_view(), name="register-user"),
     path("login/", AccountLoginView.as_view(), name="login-user"),
     path("token/refresh/", TokenRefreshView.as_view(), name="refresh-access-token"),
     path("user", AccountView.as_view(), name="account-details"),
+    path("password/change/", ChangePasswordView.as_view(), name="change-password"),
 ]
