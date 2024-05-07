c95b31cbaa170eb402f4f2b71dde52ea0a208990
Richard Quayson <richard.quayson@ashesi.edu.gh>
2024-05-07 11:27:51 +0000

    adding helper method for profile picture filename uploads

--- leerplan_api/Account/helper.py
+++ leerplan_api/Account/helper.py
@@ -1,5 +1,15 @@
+from .models import UserAccount
+
 NAME_REGEX = r"^[a-zA-Z\- ]+{2,}$"
 
 EMAIL_REGEX = r"^[^0-9!@#$%^&*(+=)\\[\].></{}`]\w+([\.-_]?\w+)*@([a-z\d-]+)\.([a-z]{2,})(\.[a-z]{2,})?$"
 
 PASSWORD_REGEX = r"^(?=(.*[A-Z]){2,})(?=(.*[a-z]){2,})(?=(.*\d){2,})(?=(.*[!#$%&()*+,-.:;<=>?@_~]){2,}).{8,}$"
+
+USERNAME_INDEX = 0      # username index in an email after splitting on the @
+
+
+def profile_picture_upload_path(instance: UserAccount, filename: str) -> str:
+    
+    username = instance.email.split('@')[USERNAME_INDEX]
+    return f"profile_picture/{username}_{filename}"
