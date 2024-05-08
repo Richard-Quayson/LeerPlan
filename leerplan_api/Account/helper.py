a7d8a844507afdc1691575bd77be16a9843cba1c
Richard Quayson <richard.quayson@ashesi.edu.gh>
2024-05-08 22:28:28 +0000

    updating name regex to resolve repeat error

--- leerplan_api/Account/helper.py
+++ leerplan_api/Account/helper.py
@@ -1,8 +1,8 @@
-NAME_REGEX = r"^[a-zA-Z\- ]+{2,}$"
+NAME_REGEX = r"^[a-zA-Z\- ]{2,}$"
 
 EMAIL_REGEX = r"^[^0-9!@#$%^&*(+=)\\[\].></{}`]\w+([\.-_]?\w+)*@([a-z\d-]+)\.([a-z]{2,})(\.[a-z]{2,})?$"
 
-PASSWORD_REGEX = r"^(?=(.*[A-Z]){2,})(?=(.*[a-z]){2,})(?=(.*\d){2,})(?=(.*[!#$%&()*+,-.:;<=>?@_~]){2,}).{8,}$"
+PASSWORD_REGEX = r"^(?=(.*[A-Z]){1,})(?=(.*[a-z]){1,})(?=(.*\d){1,})(?=(.*[!#$%&()*+,-.:;<=>?@_~]){1,}).{8,}$"
 
 USERNAME_INDEX = 0      # username index in an email after splitting on the @
 
