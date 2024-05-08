1b9a0883a8ca584c07b23f180d08cd549c142b2a
Richard Quayson <richard.quayson@ashesi.edu.gh>
2024-05-08 14:00:53 +0000

    configuring route for updating user account info

--- leerplan_api/Account/urls.py
+++ leerplan_api/Account/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
 from .views import (AccountRegistrationView, AccountLoginView, AccountView,
-                    ChangePasswordView, AccountLogoutView)
+                    UpdateAccountView, ChangePasswordView, AccountLogoutView)
 
 urlpatterns = [
     path("register/", AccountRegistrationView.as_view(), name="register-user"),
     path("login/", AccountLoginView.as_view(), name="login-user"),
     path("token/refresh/", TokenRefreshView.as_view(), name="refresh-access-token"),
     path("user/", AccountView.as_view(), name="account-details"),
+    path("user/update/", UpdateAccountView.as_view(), name="update-account"),
     path("password/change/", ChangePasswordView.as_view(), name="change-password"),
     path("logout/", AccountLogoutView.as_view(), name="logout-user"),
 ]
