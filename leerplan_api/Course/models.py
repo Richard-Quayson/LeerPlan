70ce615ad18d6d451e22ea6f6d86a88c77760773
Richard-Quayson <richard.quayson@ashesi.edu.gh>
2024-05-31 18:23:57 +0000

    adding instructor model

--- leerplan_api/Course/models.py
+++ leerplan_api/Course/models.py
@@ -18,3 +18,25 @@ class Semester(models.Model):
 
     def __str__(self):
         return f"{self.university.name} : {self.name} {self.year}"
+    
+
+class Instructor(models.Model):
+    """
+    defines an instructor model
+
+    Attributes:
+        - firstname: the first name of the instructor
+        - lastname: the last name of the instructor
+        - email: the email of the instructor
+        - phone: the phone number of the instructor
+        - type: the type of the instructor (e.g. lecturer, assistant)
+    """
+
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    email = models.EmailField()
+    phone = models.CharField(max_length=15)
+    type = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f"{self.firstname} {self.lastname} ({self.type.capitalize()}) : {self.email} ({self.phone})" 
