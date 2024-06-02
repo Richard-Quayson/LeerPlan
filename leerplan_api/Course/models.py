eb77932657617ac89e099b693b88c4978bc45abc
Richard-Quayson <richard.quayson@ashesi.edu.gh>
2024-06-02 17:48:23 +0000

    adding instructor choices to Instructor model

--- leerplan_api/Course/models.py
+++ leerplan_api/Course/models.py
@@ -19,6 +19,15 @@ class Semester(models.Model):
 
     def __str__(self):
         return f"{self.university.name} : {self.name} {self.year}"
+
+
+class InstructorType(models.TextChoices):
+    """
+    defines the instructor types
+    """
+
+    LECTURER = 'lecturer', 'Lecturer'
+    FACULTY_INTERN = 'faculty_intern', 'Faculty Intern'
     
 
 class Instructor(models.Model):
@@ -35,7 +44,7 @@ class Instructor(models.Model):
     name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
     phone = models.CharField(max_length=15, unique=True, null=True, blank=True)
-    type = models.CharField(max_length=100)
+    type = models.CharField(max_length=100, choices=InstructorType.choices)
 
     def __str__(self):
         return f"{self.name} ({self.type.capitalize()}) : {self.email} ({self.phone})" 
