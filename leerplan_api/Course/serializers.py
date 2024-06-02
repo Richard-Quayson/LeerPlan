a52143de8f7fc71862d7244fa3af1bc5a243d97c
Richard-Quayson <richard.quayson@ashesi.edu.gh>
2024-06-02 23:17:40 +0000

    adding course textbook serializer

--- leerplan_api/Course/serializers.py
+++ leerplan_api/Course/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 import re
 from .models import (
     Semester, Instructor, InstructorType, Course, CourseInstructor, CourseInstructorOfficeHour, Day,
-    CourseEvaluationCriteria, CourseLectureDay,
+    CourseEvaluationCriteria, CourseLectureDay, CourseTextbook, TextbookType,
 )
 from Account.models import University
 from Account.serializers import UniversitySerializer
@@ -334,4 +334,35 @@ class CourseLectureDaySerializer(serializers.ModelSerializer):
                                            start_time=attrs['start_time'], end_time=attrs['end_time']).exists():
             raise serializers.ValidationError("Course lecture day already exists!")
         
+        return attrs
+    
+
+class CourseTextbookSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = CourseTextbook
+        fields = ['id', 'course', 'title', 'type']
+
+    def validate_course(self, value: int) -> int:
+        if not Course.objects.filter(id=value).exists():
+            raise serializers.ValidationError("Course does not exist!")
+        
+        if Course.objects.get(id=value).is_completed:
+            raise serializers.ValidationError("You cannot add textbooks to a completed course!")
+        
+        return value
+    
+    def validate_title(self, value: str) -> str:
+        if len(value) > 255:
+            raise serializers.ValidationError("Textbook title too long!")
+        return value
+    
+    def validate_type(self, value: str) -> str:
+        if value not in TextbookType.values:
+            raise serializers.ValidationError("Invalid textbook type!")
+        return value
+    
+    def validate(self, attrs: dict) -> dict:
+        if CourseTextbook.objects.filter(course=attrs['course'], title=attrs['title']).exists():
+            raise serializers.ValidationError("Textbook already exists!")
         return attrs
