c0449d456a44756d52f6b910c154b253bf139040
Richard-Quayson <richard.quayson@ashesi.edu.gh>
2024-06-04 01:06:35 +0000

    configuring route for deleting course

--- leerplan_api/Course/urls.py
+++ leerplan_api/Course/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from .views import (
-    CreateCourseView,
+    CreateCourseView, DeleteCourseView,
 )
 
 urlpatterns = [
     path('create/', CreateCourseView.as_view(), name='create-course'),
+    path('delete/<int:course_id>/', DeleteCourseView.as_view(), name='delete-course'),
 ]
