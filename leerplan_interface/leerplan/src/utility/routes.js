acb0a4f441e56f83980e6b37a2d933d9d5b7ef72
Richard-Quayson <richard.quayson@ashesi.edu.gh>
2024-06-15 23:31:37 +0000

    adding register account route

--- leerplan_interface/leerplan/src/utility/routes.js
+++ leerplan_interface/leerplan/src/utility/routes.js
@@ -4,4 +4,6 @@ export const HOME_ROUTE = '/';                                  // Route to home
 
 export const UNDEFINED_ROUTE = '*';                             // Route to undefined page
 
+export const REGISTER_ACCOUNT_ROUTE = '/register';              // Route to register page
+
 export const LOGIN_ROUTE = '/login';                            // Route to login page
