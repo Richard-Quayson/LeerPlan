8bb2260d36ae06df6d879c38e7796afbad30e71d
Richard-Quayson <richard.quayson@ashesi.edu.gh>
2024-06-15 23:18:35 +0000

    adding user attribute ID

--- leerplan_interface/leerplan/src/utility/constants.js
+++ leerplan_interface/leerplan/src/utility/constants.js
@@ -1,8 +1,13 @@
 // access and refresh token keys
-export const ACCESS_TOKEN = 'access';
-export const REFRESH_TOKEN = 'refresh';
+export const ACCESS_TOKEN = "access";
+export const REFRESH_TOKEN = "refresh";
+
 
 // name, email and password regex
 NAME_REGEX = /^[a-zA-Z\- ]{2,}$/;
 EMAIL_REGEX = /^[^0-9!@#$%^&*(+=)\\[\].></{}`]\w+([\.-_]?\w+)*@([a-z\d-]+)\.([a-z]{2,})(\.[a-z]{2,})?$/;
 PASSWORD_REGEX = /^(?=(.*[A-Z]){1,})(?=(.*[a-z]){1,})(?=(.*\d){1,})(?=(.*[!#$%&()*+,-.:;<=>?@_~]){1,}).{8,}$/;
+
+
+// user attribute constants
+export const CURRENT_USER_ID = "CURRENT_USER_ID";
