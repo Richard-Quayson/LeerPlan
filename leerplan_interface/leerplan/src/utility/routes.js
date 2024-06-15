403fb73fadf60548c806e436656b078c54e6f3ca
Richard-Quayson <richard.quayson@ashesi.edu.gh>
2024-06-15 22:25:34 +0000

    adding essential routes

---
+++ leerplan_interface/leerplan/src/utility/routes.js
@@ -0,0 +1,7 @@
+// APP ROUTES //
+
+export const HOME_ROUTE = '/';                                  // Route to home page
+
+export const UNDEFINED_ROUTE = '*';                             // Route to undefined page
+
+export const LOGIN_ROUTE = '/login';                            // Route to login page
