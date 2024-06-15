7738a1834ced0a1bf7e2dfd7ce4a3c5d6b72bafd
Richard-Quayson <richard.quayson@ashesi.edu.gh>
2024-06-15 23:51:23 +0000

    adding logout and dashboard route

--- leerplan_interface/leerplan/src/utility/routes.js
+++ leerplan_interface/leerplan/src/utility/routes.js
@@ -7,3 +7,7 @@ export const UNDEFINED_ROUTE = '*';                             // Route to unde
 export const REGISTER_ACCOUNT_ROUTE = '/register';              // Route to register page
 
 export const LOGIN_ROUTE = '/login';                            // Route to login page
+
+export const LOGOUT_ROUTE = '/logout';                          // Route to logout page
+
+export const DASHBOARD_ROUTE = '/dashboard';                    // Route to dashboard page
