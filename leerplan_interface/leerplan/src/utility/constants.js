c9fb5a1554843b1a87fd8288fe5dbd719d778d1a
Richard-Quayson <richard.quayson@ashesi.edu.gh>
2024-06-16 00:58:47 +0000

    resolving error with regex exports

--- leerplan_interface/leerplan/src/utility/constants.js
+++ leerplan_interface/leerplan/src/utility/constants.js
@@ -4,9 +4,9 @@ export const REFRESH_TOKEN = "refresh";
 
 
 // name, email and password regex
-NAME_REGEX = /^[a-zA-Z\- ]{2,}$/;
-EMAIL_REGEX = /^[^0-9!@#$%^&*(+=)\\[\].></{}`]\w+([\.-_]?\w+)*@([a-z\d-]+)\.([a-z]{2,})(\.[a-z]{2,})?$/;
-PASSWORD_REGEX = /^(?=(.*[A-Z]){1,})(?=(.*[a-z]){1,})(?=(.*\d){1,})(?=(.*[!#$%&()*+,-.:;<=>?@_~]){1,}).{8,}$/;
+export const NAME_REGEX = /^[a-zA-Z\- ]{2,}$/;
+export const EMAIL_REGEX = /^[^0-9!@#$%^&*(+=)\\[\].></{}`]\w+([\.-_]?\w+)*@([a-z\d-]+)\.([a-z]{2,})(\.[a-z]{2,})?$/;
+export const PASSWORD_REGEX = /^(?=(.*[A-Z]){1,})(?=(.*[a-z]){1,})(?=(.*\d){1,})(?=(.*[!#$%&()*+,-.:;<=>?@_~]){1,}).{8,}$/;
 
 
 // user attribute constants
