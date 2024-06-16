66d0a84bfcca2ae880c1d4838f0d108b6e84ebd3
Richard-Quayson <richard.quayson@ashesi.edu.gh>
2024-06-16 02:27:38 +0000

    updating dashboard page to add dummy logout button

--- leerplan_interface/leerplan/src/pages/DashboardPage.jsx
+++ leerplan_interface/leerplan/src/pages/DashboardPage.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 
 const DashboardPage = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    navigate("/logout");
+  }
+
   return (
     <div>
       <h1>DashBoard Page</h1>
+      <button onClick={handleLogout}>Logout</button>
     </div>
   );
 };
