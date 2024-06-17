dd18e7c68fe897f8aab647e95a064c82eb7e9678
Richard-Quayson <richard.quayson@ashesi.edu.gh>
2024-06-17 02:14:58 +0000

    updating dashboard page to render left pane component

--- leerplan_interface/leerplan/src/pages/DashboardPage.jsx
+++ leerplan_interface/leerplan/src/pages/DashboardPage.jsx
@@ -1,18 +1,51 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import ProtectedRoute from "../components/ProtectedRoute";
+import LeftPane from "../components/LeftPane";
+import { CURRENT_USER_ID } from "../utility/constants";
+import { USER_DETAILS_URL } from "../utility/api_urls";
+import api from "../utility/api"; 
 
 const DashboardPage = () => {
-  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  const handleLogout = () => {
-    navigate("/logout");
+  useEffect(() => {
+    const fetchUserDetails = async () => {
+      try {
+        const response = await api.get(USER_DETAILS_URL);
+        setUser(response.data);
+        localStorage.setItem(CURRENT_USER_ID, response.data.id);
+      } catch (error) {
+        setError(error.message || "Failed to fetch user details");
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchUserDetails();
+  }, []);
+
+  if (isLoading) {
+    return <div>Loading...</div>;
   }
 
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
+  console.log(user);
+
   return (
-    <div>
-      <h1>DashBoard Page</h1>
-      <button onClick={handleLogout}>Logout</button>
-    </div>
+    <ProtectedRoute>
+      <div className="h-screen overflow-y-auto flex">
+        <div className="w-1/4">
+          <LeftPane user={user} />
+        </div>
+
+        <div className="w-3/4 pr-4"></div>
+      </div>
+    </ProtectedRoute>
   );
 };
 
