7b3f5095cb216c364d88f646f4202e05f3291277
Richard-Quayson <richard.quayson@ashesi.edu.gh>
2024-06-17 04:47:07 +0000

    updating add new university modal

--- leerplan_interface/leerplan/src/components/AddNewUniversityModal.jsx
+++ leerplan_interface/leerplan/src/components/AddNewUniversityModal.jsx
@@ -3,6 +3,7 @@ import ModalContainer from "./ModalContainer";
 import api from "../utility/api";
 import { ADD_USER_UNIVERSITY_URL } from "../utility/api_urls";
 import { NAME_REGEX } from "../utility/constants";
+import UniversityIcon from "../assets/icons/University.png";
 import SuccessGif from "../assets/gifs/Success.gif";
 
 const AddNewUniversityModal = ({ isOpen, onClose, onUniversityAdded }) => {
@@ -10,11 +11,12 @@ const AddNewUniversityModal = ({ isOpen, onClose, onUniversityAdded }) => {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const [message, setMessage] = useState("");
   const [isSuccess, setIsSuccess] = useState(false);
+  const [isNameValid, setIsNameValid] = useState(false);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    if (!NAME_REGEX.test(universityName)) {
+    if (!isNameValid) {
       setMessage("Invalid university name.");
       setTimeout(() => {
         setMessage("");
@@ -29,7 +31,7 @@ const AddNewUniversityModal = ({ isOpen, onClose, onUniversityAdded }) => {
         name: universityName,
       });
 
-      if (response.status === 200 || response.status === 201) {
+      if (response.status === 201) {
         setIsSuccess(true);
         setTimeout(() => {
           onUniversityAdded();
@@ -46,16 +48,22 @@ const AddNewUniversityModal = ({ isOpen, onClose, onUniversityAdded }) => {
     }
   };
 
+  const handleUniversityNameChange = (event) => {
+    const value = event.target.value;
+    setUniversityName(value);
+    setIsNameValid(value.trim() !== "" && NAME_REGEX.test(value));
+  };
+
   return (
     <ModalContainer isOpen={isOpen} onClose={onClose}>
       <h2 className="text-xl font-semibold mb-4 text-center text-yellow-800">
         Add New University
       </h2>
-      <p className="text-center mb-4">
+      <p className="text-center text-gray-500 mb-4">
         Please enter the name of the university you wish to add.
       </p>
       {message && (
-        <div className="text-center mb-4 text-red-500 font-semibold">
+        <div className="text-center mb-4 text-red-500">
           {message}
         </div>
       )}
@@ -68,23 +76,37 @@ const AddNewUniversityModal = ({ isOpen, onClose, onUniversityAdded }) => {
         </div>
       ) : (
         <form onSubmit={handleSubmit}>
-          <div className="mb-4">
-            <label htmlFor="universityName" className="block text-gray-700">
+          <div className="relative mb-4">
+            <label
+              htmlFor="universityName"
+              className="block text-sm font-medium text-gray-700"
+            >
               University Name
             </label>
-            <input
-              type="text"
-              id="universityName"
-              value={universityName}
-              onChange={(e) => setUniversityName(e.target.value)}
-              className="w-full p-2 border rounded-md"
-              required
-            />
+            <div className="relative mt-1">
+              <input
+                type="text"
+                id="universityName"
+                name="name"
+                value={universityName}
+                onChange={handleUniversityNameChange}
+                className={`block w-full pl-10 pr-3 py-2 border rounded-md shadow-sm sm:text-sm ${
+                  universityName
+                    ? isNameValid
+                      ? "border-green-500"
+                      : "border-red-500"
+                    : "border-gray-300"
+                } focus:outline-none`}
+              />
+              <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                <img src={UniversityIcon} alt="Name Icon" className="h-5 w-5" />
+              </div>
+            </div>
           </div>
           <div className="flex justify-center">
             <button
               type="submit"
-              className="px-4 py-2 bg-blue-600 text-white rounded-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-600"
+              className="px-4 py-2 border-[1px] border-yellow-800 text-black rounded-md hover:bg-yellow-800 hover:text-white focus:outline-none focus:ring-2 focus:ring-blue-600"
               disabled={isSubmitting}
             >
               {isSubmitting ? "Submitting..." : "Add University"}
