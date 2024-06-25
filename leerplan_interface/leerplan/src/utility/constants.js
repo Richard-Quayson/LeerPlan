339b5922335fc60414955d5cdbb7aacbcd1a5148
Richard Quayson <richard.quayson@ashesi.edu.gh>
2024-06-25 21:41:03 +0000

    adding course and routine display list constants

--- leerplan_interface/leerplan/src/utility/constants.js
+++ leerplan_interface/leerplan/src/utility/constants.js
@@ -13,5 +13,5 @@ export const PASSWORD_REGEX = /^(?=(.*[A-Z]){1,})(?=(.*[a-z]){1,})(?=(.*\d){1,})
 export const CURRENT_USER_ID = "leerplan:CURRENT_USER_ID";
 export const PREFERRED_UNIVERSITY_NAME = "leerplan:PREFERRED_UNIVERSITY_NAME";
 export const PREFERRED_UNIVERSITY_ID = "leerplan:PREFERRED_UNIVERSITY_ID";
-export const EXTENDED_COURSE_LIST = "leerplan:EXTENDED_COURSE_LIST";
-export const EXTENDED_ROUTINE_LIST = "leerplan:EXTENDED_ROUTINE_LIST";
+export const EXTENDED_COURSE_LIST_DISPLAY = "leerplan:EXTENDED_COURSE_LIST_DISPLAY";
+export const EXTENDED_ROUTINE_LIST_DISPLAY = "leerplan:EXTENDED_ROUTINE_LIST_DISPLAY";
