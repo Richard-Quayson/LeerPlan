4487f3b6565466b55023b748a4d31ebb0c334364
Richard Quayson <richard.quayson@ashesi.edu.gh>
2024-06-27 02:01:10 +0000

    configuring route for retrieving user courses

--- leerplan_api/Course/urls.py
+++ leerplan_api/Course/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from .views import (
-    CreateCourseView, DeleteCourseView,
+    CreateCourseView, RetrieveUserCoursesView, DeleteCourseView,
 )
 
 urlpatterns = [
     path('create/', CreateCourseView.as_view(), name='create-course'),
+    path('user/', RetrieveUserCoursesView.as_view(), name='retrieve-user-courses'),
     path('delete/<int:course_id>/', DeleteCourseView.as_view(), name='delete-course'),
 ]
