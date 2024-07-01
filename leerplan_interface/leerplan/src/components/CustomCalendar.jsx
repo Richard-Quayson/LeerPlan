4268fc9109b173d073d3584e6a813c2dc6a72b20
Richard-Quayson <richard.quayson@ashesi.edu.gh>
2024-07-01 23:58:01 +0000

    updating calender component to render course colors and general styling

--- leerplan_interface/leerplan/src/components/CustomCalendar.jsx
+++ leerplan_interface/leerplan/src/components/CustomCalendar.jsx
@@ -1,12 +1,33 @@
-import React from "react";
 import { Calendar, momentLocalizer } from "react-big-calendar";
 import moment from "moment";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import { COURSE_ROUTINE_COLOURS } from "../utility/constants";
 
 const localizer = momentLocalizer(moment);
 
+const getLastCourseEndDate = (courses) => {
+  let latestEndDate = null;
+  courses.forEach((course) => {
+    course.course.lecture_days.forEach((lecture) => {
+      const end = moment(
+        `${lecture.day} ${lecture.end_time}`,
+        "dddd HH:mm:ss"
+      ).toDate();
+      if (!latestEndDate || end > latestEndDate) {
+        latestEndDate = end;
+      }
+    });
+  });
+  return latestEndDate;
+};
+
+const getColorForEvent = (index) => {
+  const colors = Object.values(COURSE_ROUTINE_COLOURS);
+  return colors[index % colors.length].light;
+};
+
 const CustomCalendar = ({ courses }) => {
-  const events = courses.flatMap((course) => {
+  const events = courses.flatMap((course, courseIndex) => {
     return course.course.lecture_days.map((lecture) => {
       const start = moment(
         `${lecture.day} ${lecture.start_time}`,
@@ -22,23 +43,25 @@ const CustomCalendar = ({ courses }) => {
         start,
         end,
         allDay: false,
+        color: getColorForEvent(courseIndex),
         repeating: {
           freq: "WEEKLY",
-          until: new Date(2022, 4, 29),
+          until: getLastCourseEndDate(courses),
         },
       };
     });
   });
 
   const eventStyleGetter = (event) => {
-    const backgroundColor = event.color;
     const style = {
-      backgroundColor,
+      backgroundColor: event.color,
       borderRadius: "5px",
-      opacity: 0.8,
       color: "white",
       border: "0px",
       display: "block",
+      fontSize: "14px",
+      fontWeight: "bold",
+      fontFamily: "sans-serif",
     };
     return {
       style,
@@ -46,12 +69,14 @@ const CustomCalendar = ({ courses }) => {
   };
 
   return (
-    <div className="h-full">
+    <div className="h-full sans-serif text-sm">
       <Calendar
         localizer={localizer}
         events={events}
         startAccessor="start"
         endAccessor="end"
+        views={["month", "week", "day"]}
+        defaultView="week"
         style={{ height: "calc(100vh - 80px)" }}
         eventPropGetter={eventStyleGetter}
       />
