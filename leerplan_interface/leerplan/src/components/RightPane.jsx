1342fea4850e8323f8b6c2695c9363da0d0b8478
Richard-Quayson <richard.quayson@ashesi.edu.gh>
2024-07-01 21:54:48 +0000

    updating right pane component to conditionally render courses

--- leerplan_interface/leerplan/src/components/RightPane.jsx
+++ leerplan_interface/leerplan/src/components/RightPane.jsx
@@ -7,7 +7,13 @@ const RightPane = ({ courses }) => {
     <div className="flex flex-col h-full">
       <HorizontalNavigation title="Dashboard" />
       <div className="flex-grow pl-8 bg-white">
-        <CustomCalendar courses={courses} />
+        {courses && courses.length > 0 ? (
+          <CustomCalendar courses={courses} />
+        ) : (
+          <div className="h-full flex items-center justify-center">
+            <h1 className="text-2xl text-gray-400">No courses found</h1>
+          </div>
+        )}
       </div>
     </div>
   );
