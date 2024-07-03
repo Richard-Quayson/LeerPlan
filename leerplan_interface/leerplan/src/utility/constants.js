eb29d927604b792725c4a5ca43c3f06889c3b686
Richard-Quayson <richard.quayson@ashesi.edu.gh>
2024-07-03 03:05:28 +0000

    updating user preferences description

--- leerplan_interface/leerplan/src/utility/constants.js
+++ leerplan_interface/leerplan/src/utility/constants.js
@@ -7,7 +7,7 @@ export const NAME_REGEX = /^[a-zA-Z\- ]{2,}$/;
 export const EMAIL_REGEX = /^[^0-9!@#$%^&*(+=)\\[\].></{}`]\w+([\.-_]?\w+)*@([a-z\d-]+)\.([a-z]{2,})(\.[a-z]{2,})?$/;
 export const PASSWORD_REGEX = /^(?=(.*[A-Z]){1,})(?=(.*[a-z]){1,})(?=(.*\d){1,})(?=(.*[!#$%&()*+,-.:;<=>?@_~]){1,}).{8,}$/;
 
-// user attribute constants
+// user preferences
 export const CURRENT_USER_ID = "leerplan:CURRENT_USER_ID";
 export const PREFERRED_UNIVERSITY_NAME = "leerplan:PREFERRED_UNIVERSITY_NAME";
 export const PREFERRED_UNIVERSITY_ID = "leerplan:PREFERRED_UNIVERSITY_ID";
