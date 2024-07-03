b7efefd8115c6a1d492ff4c649df15e0443076c7
Richard-Quayson <richard.quayson@ashesi.edu.gh>
2024-07-03 23:00:40 +0000

    updating course summary to redirect to course page on click

--- leerplan_interface/leerplan/src/components/CourseSummaryCard.jsx
+++ leerplan_interface/leerplan/src/components/CourseSummaryCard.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
+import { COURSE_ROUTE } from "../utility/routes";
 
 const truncateText = (text, maxLength) => {
   if (text.length > maxLength) {
@@ -11,10 +13,17 @@ const CourseSummaryCard = ({ code, title, color }) => {
   const maxTextLength = 50;
   const truncatedTitle = truncateText(`${code} - ${title}`, maxTextLength);
 
+  const navigate = useNavigate();
+
+  const handleCardClick = () => {
+    navigate(COURSE_ROUTE, { state: { courseCode: code } });
+  };
+
   return (
     <div
-      className="relative flex items-center mt-3 rounded-l-md overflow-hidden"
+      className="relative flex items-center mt-3 rounded-l-md overflow-hidden cursor-pointer"
       style={{ backgroundColor: color.light }}
+      onClick={handleCardClick}
     >
       <div
         className="absolute left-0 top-0 bottom-0 w-[5px]"
