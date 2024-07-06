d3aba5deeed7b63f4268cfe3709e8813fdaae258
Richard-Quayson <richard.quayson@ashesi.edu.gh>
2024-07-06 22:46:11 +0000

    feat: Define route for SpecifyCourseCohortView endpoint

--- leerplan_api/Course/urls.py
+++ leerplan_api/Course/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from .views import (
-    CreateCourseView, RetrieveUserCoursesView, DeleteCourseView,
+    CreateCourseView, RetrieveUserCoursesView, SpecifyCourseCohortView, DeleteCourseView,
 )
 
 urlpatterns = [
     path('create/', CreateCourseView.as_view(), name='create-course'),
     path('user/', RetrieveUserCoursesView.as_view(), name='retrieve-user-courses'),
+    path('cohort/specify/', SpecifyCourseCohortView.as_view(), name='specify-course-cohort'),
     path('delete/<int:course_id>/', DeleteCourseView.as_view(), name='delete-course'),
 ]
