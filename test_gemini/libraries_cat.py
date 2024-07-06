942df0c764a6b70383dfbe2fe309a92c855e0e5e
Richard-Quayson <richard.quayson@ashesi.edu.gh>
2024-07-06 18:59:38 +0000

    chore: Add pip commands for dependencies in requirements.txt

---
+++ test_gemini/libraries_cat.py
@@ -0,0 +1,14 @@
+# pip install pydantic
+"annotated-types==0.6.0"
+"pydantic==2.6.4"
+"pydantic_core==2.16.3"
+"typing_extensions==4.10.0"
+
+# pip install PyMuPDF
+"PyMuPDF==1.23.26"
+"PyMuPDFb==1.23.22"
+
+# pip install python-dotenv
+"python-dotenv==1.0.1"
+
+# 
