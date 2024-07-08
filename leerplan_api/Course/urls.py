c9f7d6814901817d9b675c2ab88a939222c074bc
Richard-Quayson <richard.quayson@ashesi.edu.gh>
2024-07-08 04:25:09 +0000

    feat: Add DetermineTimeChunksView for generating free time slots

--- leerplan_api/Course/urls.py
+++ leerplan_api/Course/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import (
     CreateCourseView, RetrieveUserCoursesView, SpecifyCourseCohortView, DeleteCourseView,
+    DetermineTimeChunksView,
 )
 
 urlpatterns = [
@@ -8,4 +9,5 @@ urlpatterns = [
     path('user/', RetrieveUserCoursesView.as_view(), name='retrieve-user-courses'),
     path('cohort/specify/', SpecifyCourseCohortView.as_view(), name='specify-course-cohort'),
     path('delete/<int:course_id>/', DeleteCourseView.as_view(), name='delete-course'),
+    path('timechunks/', DetermineTimeChunksView.as_view(), name='determine-time-chunks'),
 ]
