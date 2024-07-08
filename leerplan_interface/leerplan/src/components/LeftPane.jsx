3f85913bcbf32bf566ec162ca80dd8b6091abbe2
Richard-Quayson <richard.quayson@ashesi.edu.gh>
2024-07-08 23:34:41 +0000

    feat: Add Generate Schedule functionality

--- leerplan_interface/leerplan/src/components/LeftPane.jsx
+++ leerplan_interface/leerplan/src/components/LeftPane.jsx
@@ -4,15 +4,37 @@ import { Tooltip } from "react-tooltip";
 import UserProfileCard from "./UserProfileCard";
 import CourseList from "./CourseList";
 import RoutineList from "./RoutineList";
+import AddUserMetaData from "./AddUserMetaData";
+import YesNoPrompt from "./YesNoPrompt";
 import { LOGOUT_ROUTE } from "../utility/routes";
 import { CURRENT_USER_ID } from "../utility/constants";
 import LeerPlanLogo from "../assets/images/leerplanlogo.png";
+import AutomateIcon from "../assets/icons/Automate.png";
 import FilledLogoutIcon from "../assets/icons/FilledLogout.png";
 import UnfilledLogoutIcon from "../assets/icons/UnfilledLogout.png";
 
 const LeftPane = ({ user, userCourses }) => {
   const navigate = useNavigate();
   const [isHovered, setIsHovered] = useState(false);
+  const [showYesNoPrompt, setShowYesNoPrompt] = useState(false);
+  const [showAddMetadataForm, setShowAddMetadataForm] = useState(false);
+
+  const handleGenerateSchedule = () => {
+    if (!user.metadata) {
+      setShowAddMetadataForm(true);
+    } else {
+      setShowYesNoPrompt(true);
+    }
+  };
+
+  const handleYesResponse = () => {
+    setShowYesNoPrompt(false);
+    window.location.reload();
+  };
+
+  const handleNoResponse = () => {
+    setShowYesNoPrompt(false);
+  };
 
   return (
     <div className="left-pane h-full flex flex-col">
@@ -33,7 +55,7 @@ const LeftPane = ({ user, userCourses }) => {
           <UserProfileCard user={user} />
         </div>
 
-        <hr className="border-gray-200 w-full" />
+        <hr className="border-gray-300 w-full" />
       </div>
 
       <div className="flex-grow overflow-y-auto w-full">
@@ -43,20 +65,38 @@ const LeftPane = ({ user, userCourses }) => {
         </div>
 
         {/* LINE SEPARATOR */}
-        <hr className="border-gray-200 w-full mt-4" />
+        <hr className="border-gray-300 w-full mt-4" />
 
         {/* ROUTINE DIRECTORY */}
         <div>
           <RoutineList routines={user.routines} />
         </div>
+
+        {/* LINE SEPARATOR */}
+        <hr className="border-gray-300 w-full mt-4" />
+
+        {/* GENERATE ACADEMIC CALENDAR */}
+        <div className="mt-8 flex justify-center">
+          <button
+            onClick={handleGenerateSchedule}
+            className="w-[200px] py-2 border border-yellow-800 text-yellow-800 rounded-lg"
+          >
+            <img
+              src={AutomateIcon}
+              alt="Automate Icon"
+              className="w-6 h-6 inline-block mr-2"
+            />
+            Generate Schedule
+          </button>
+        </div>
       </div>
 
       {/* LINE SEPARATOR */}
-      <hr className="border-gray-200 w-full mt-4" />
+      <hr className="border-gray-300 w-full mt-4" />
 
       {/* LOGOUT */}
       <div
-        className="logout-container"
+        className="logout-container mt-auto"
         onMouseOver={() => setIsHovered(true)}
         onMouseOut={() => setIsHovered(false)}
       >
@@ -77,6 +117,25 @@ const LeftPane = ({ user, userCourses }) => {
       </div>
 
       <Tooltip id="logout" place="right" />
+
+      {/* YES/NO PROMPT */}
+      {showYesNoPrompt && (
+        <YesNoPrompt
+          question="Generating an academic calendar requires all course syllabus for the semester. Have you uploaded the necessary courses and created your routines?"
+          onYes={handleYesResponse}
+          onNo={handleNoResponse}
+        />
+      )}
+
+      {/* ADD METADATA FORM */}
+      {showAddMetadataForm && (
+        <AddUserMetaData
+          isOpen={showAddMetadataForm}
+          onClose={() => setShowAddMetadataForm(false)}
+          metadata={{}}
+          editMode={false}
+        />
+      )}
     </div>
   );
 };
