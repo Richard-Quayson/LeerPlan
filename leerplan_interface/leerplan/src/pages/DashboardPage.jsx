5f78f29b2a32a512cf8c16f9d77b2aed9937cb6f
Richard-Quayson <richard.quayson@ashesi.edu.gh>
2024-07-28 00:02:52 +0000

    refactor: Update loading spinner style in DashboardPage
    The loading spinner style in the DashboardPage component has been updated to use a rounded-2xl background instead of a rounded-lg background. This change improves the visual appearance of the loading spinner.

--- leerplan_interface/leerplan/src/pages/DashboardPage.jsx
+++ leerplan_interface/leerplan/src/pages/DashboardPage.jsx
@@ -83,7 +83,7 @@ const DashboardPage = () => {
 
         {isLoading && (
           <div className="fixed inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center z-50">
-            <div className="bg-white rounded-lg">
+            <div className="bg-white rounded-2xl">
               <img src={LoadingGif} alt="Loading" className="w-32 h-32" />
             </div>
           </div>
